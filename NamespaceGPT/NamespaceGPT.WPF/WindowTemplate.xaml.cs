4118d6e4a233ef4665a531fa605e91fb7e8dce0d
MogaDenis <moga_denis@yahoo.com>
2024-04-09 20:30:17 +0300

    Added ProductReviews page

--- NamespaceGPT/NamespaceGPT.WPF/WindowTemplate.xaml.cs
+++ NamespaceGPT/NamespaceGPT.WPF/WindowTemplate.xaml.cs
@@ -12,13 +12,16 @@ namespace NamespaceGPT.WPF
 
         public void ShowFavouriteProductsView()
         {
-            
 
-            FavouriteProductsView favouriteProductsView = new(Session.GetInstance().UserId);
-            MainFrame.NavigationService.Navigate(favouriteProductsView);
+
+            //FavouriteProductsView favouriteProductsView = new(Session.GetInstance().UserId);
+            //MainFrame.NavigationService.Navigate(favouriteProductsView);
 
             //CompareProductsView compareView = new CompareProductsView(p1, p2);
             //MainFrame.NavigationService.Navigate(compareView);
+
+            ProductReviewsPage productReviews = new ProductReviewsPage(1);
+            MainFrame.NavigationService.Navigate(productReviews);
         }
     }
 }
